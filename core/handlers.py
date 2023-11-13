061721a51e7f0bf84d56980cda1541b8650ba28f
funnydevelopment <ya.giggs2013@yandex.ru>
2023-11-14 01:40:05 +0300

    added cyrillic checker

--- core/handlers.py
+++ core/handlers.py
@@ -7,6 +7,7 @@ from aiogram.fsm.context import FSMContext
 from .states import PhraseBotState
 from . import message_texts as texts
 from . import keyboards as kb
+from . import utils
 
 router = Router()
 
@@ -80,20 +81,35 @@ async def decline_user_data(message: types.Message, state: FSMContext):
 
 @router.message(PhraseBotState.first_word)
 async def get_first_word(message: types.Message, state: FSMContext):
-    await state.update_data(first_word=message.text.lower())
-    await message.answer(text=texts.SECOND_WORD_TEXT)
-    await state.set_state(state=PhraseBotState.second_word)
+    is_valid_word = await utils.is_cyrillic_with_hyphen_string(message.text)
+    if is_valid_word:
+        await state.update_data(first_word=message.text.lower())
+        await message.answer(text=texts.SECOND_WORD_TEXT)
+        await state.set_state(state=PhraseBotState.second_word)
+    else:
+        await message.answer(text=texts.NOT_VALID_TEXT)
+        await state.set_state(PhraseBotState.first_word)
 
 
 @router.message(PhraseBotState.second_word)
 async def get_second_word(message: types.Message, state: FSMContext):
-    await state.update_data(second_word=message.text.lower())
-    await message.answer(text=texts.NAME_INPUT_TEXT)
-    await state.set_state(state=PhraseBotState.input_name)
+    is_valid_word = await utils.is_cyrillic_with_hyphen_string(message.text)
+    if is_valid_word:
+        await state.update_data(second_word=message.text.lower())
+        await message.answer(text=texts.NAME_INPUT_TEXT)
+        await state.set_state(state=PhraseBotState.input_name)
+    else:
+        await message.answer(text=texts.NOT_VALID_TEXT)
+        await state.set_state(state=PhraseBotState.second_word)
 
 
 @router.message(PhraseBotState.input_name)
 async def get_user_name(message: types.Message, state: FSMContext):
-    await state.update_data(user_name=message.text, user_id=message.from_user.id)
-    await state.set_state(PhraseBotState.accept_state)
-    await get_all_data(message, state)
+    is_valid_word = await utils.is_cyrillic_with_hyphen_string(message.text)
+    if is_valid_word:
+        await state.update_data(user_name=message.text, user_id=message.from_user.id)
+        await state.set_state(PhraseBotState.accept_state)
+        await get_all_data(message, state)
+    else:
+        await message.answer(text=texts.NOT_VALID_TEXT)
+        await state.set_state(state=PhraseBotState.input_name)
