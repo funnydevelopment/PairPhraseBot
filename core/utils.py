c2b2a515307c480ef0c3f9d613566d133729d53d
funnydevelopment <ya.giggs2013@yandex.ru>
2023-11-14 16:21:12 +0300

    updated cyrillic checker

--- core/utils.py
+++ core/utils.py
@@ -2,7 +2,7 @@ import re
 
 
 async def is_cyrillic_with_hyphen_string(input_row: str) -> bool:
-    pattern = re.compile(r"^[а-яА-Я-]+$")
+    pattern = re.compile(r"^[а-яА-ЯӨөҮүҢң-]+$")
     if pattern.match(input_row):
         return True
     return False
