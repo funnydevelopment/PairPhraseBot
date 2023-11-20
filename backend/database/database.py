eef2f4eb2565ddf925639e09c13cb066072e872b
funnydevelopment <ya.giggs2013@yandex.ru>
2023-11-20 17:04:59 +0300

    created database session

---
+++ backend/database/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import as_declarative
+
+from .config import settings
+
+
+engine = create_async_engine(settings.db_url)
+
+async_session_make = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+@as_declarative()
+class Base:
+    metadata = MetaData()
