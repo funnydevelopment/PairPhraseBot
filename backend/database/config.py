63ed12a7d73d1713a6b3b6d9591f34e5f30d7f29
funnydevelopment <ya.giggs2013@yandex.ru>
2023-11-20 16:55:57 +0300

    updated database config

--- backend/database/config.py
+++ backend/database/config.py
@@ -1,25 +1,18 @@
-from sqlalchemy import MetaData
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker, as_declarative
+from dataclasses import dataclass
 
 from environs import Env
+
 
 env = Env()
 env.read_env(".env")
 
-DB_HOST = env.str("DB_HOST")
-DB_PORT = env.str("DB_PORT")
-DB_USER = env.str("DB_USER")
-DB_NAME = env.str("DB_NAME")
-DB_PASS = env.str("DB_PASS")
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+@dataclass(frozen=True, slots=True)
+class Settings:
+    user = f'{env.str("DB_USER")}:{env.str("DB_PASS")}'
+    database = f'{env.str("DB_HOST")}:{env.str("DB_PORT")}/{env.str("DB_NAME")}'
 
-engine = create_async_engine(DATABASE_URL)
-
-async_session_make = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    db_url = f"postgresql+asyncpg://{user}@{database}"
 
 
-@as_declarative()
-class Base:
-    metadata = MetaData()
+settings = Settings()
